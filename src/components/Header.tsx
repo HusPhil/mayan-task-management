574d0c8ddd8afeba5ed50484307dbde1d8ebd637
HusPhil009 <husphil200@gmail.com>
2026-08-18 20:59:05 +0800

    style: enhance Header component layout with flexbox and improved styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,14 @@
 import { ListTodoIcon } from "lucide-react"
-import React from "react"
+import CreateTaskSheet from "./sheets/CreateTaskSheet"
 
 export default function Header() {
   return (
-    <div>
-      <ListTodoIcon />
-      <h1>Mayan Tasks</h1>
+    <div className="flex items-center justify-between border px-1 py-5">
+      <div className="flex items-center gap-2">
+        <ListTodoIcon size={30} />
+        <h1 className="text-2xl font-semibold">Mayan Tasks</h1>
+      </div>
+      <CreateTaskSheet />
     </div>
   )
 }
