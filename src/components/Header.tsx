bdf6daf203e5f915e85c23774a8883be13396976
HusPhil009 <husphil200@gmail.com>
2026-08-18 19:43:15 +0800

    setup: structure App component with Header and TaskList

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { ListTodoIcon } from "lucide-react"
+import React from "react"
+
+export default function Header() {
+  return (
+    <div>
+      <ListTodoIcon />
+      <h1>Mayan Tasks</h1>
+    </div>
+  )
+}
